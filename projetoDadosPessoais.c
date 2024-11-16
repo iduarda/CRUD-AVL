57e13187aeeeb1d752d2b4e31bd9cfa947b5d749
Maria Eduarda <142428634+iduarda@users.noreply.github.com>
2024-11-16 01:28:04 -0300

    Update projetoDadosPessoais.c

--- projetoDadosPessoais.c
+++ projetoDadosPessoais.c
@@ -18,7 +18,7 @@ typedef struct Estrutura {
 	struct Estrutura *dir;
 } Estrutura;
 
-int calculaAltura(Estrutura *no){
+int calculaAltura(Estrutura *no) {
 	int alturaDir = 0;
 	int alturaEsq = 0;
 	
@@ -47,7 +47,7 @@ Estrutura *rotacaoEsq(Estrutura *antigaRaiz) {
 	Estrutura *novaRaiz = antigaRaiz->dir;
 	Estrutura *aux = novaRaiz->esq;
 	novaRaiz->esq = antigaRaiz;
-	antigaRaiz->esq = aux;
+	antigaRaiz->dir = aux;
 	
 	return novaRaiz;
 }
@@ -69,6 +69,17 @@ int fatorBalanc(Estrutura *no) {
 	return no ? calculaAltura(no->esq) - calculaAltura(no->dir) : 0;
 }
 
+int validarCPF(unsigned long long cpf) {
+	int digitos = 0;
+	
+	while(cpf) {
+		cpf /= 10;
+		digitos++;
+	}
+	
+	return digitos == 11;
+}
+
 Estrutura *inserir(Estrutura *raiz, Registro dado) {
 	if(!raiz) {
 		raiz = (Estrutura *)malloc(sizeof(Estrutura));
@@ -76,7 +87,7 @@ Estrutura *inserir(Estrutura *raiz, Registro dado) {
 		raiz->altura = 0;
 		raiz->esq = NULL;
 		raiz->dir = NULL;
-	} else if(dado.cpf < raiz->dado.cpf) {
+	}else if(dado.cpf < raiz->dado.cpf) {
 		raiz->esq = inserir(raiz->esq, dado);
 		
 		if(fatorBalanc(raiz) == 2) {
@@ -85,7 +96,7 @@ Estrutura *inserir(Estrutura *raiz, Registro dado) {
 			else
 				raiz = rotacaoDuplaDir(raiz);
 		}
-	} else if(dado.cpf > raiz->dado.cpf) {
+	}else if(dado.cpf > raiz->dado.cpf) {
 		raiz->dir = inserir(raiz->dir, dado);
 		if(fatorBalanc(raiz) == -2) {
 			if(dado.cpf > raiz->dir->dado.cpf)
@@ -93,8 +104,9 @@ Estrutura *inserir(Estrutura *raiz, Registro dado) {
 			else
 				raiz = rotacaoDuplaDir(raiz);
 		}
-	} else
-		printf("ERRO! CPF j· existe!");
+	}else {
+		printf("ERRO! O CPF %llu j√° existe!\n", dado.cpf);
+	}
 	
 	raiz->altura = calculaAltura(raiz);
 	
@@ -104,6 +116,7 @@ Estrutura *inserir(Estrutura *raiz, Registro dado) {
 Estrutura *busca(Estrutura *raiz, unsigned long long cpf) {
 	if(!raiz)
 		return NULL;
+		
 	if(cpf < raiz->dado.cpf)
 		return busca(raiz->esq, cpf);
 	else if(cpf > raiz->dado.cpf)
@@ -121,7 +134,7 @@ Estrutura *menor(Estrutura *no) {
 
 Estrutura *remover(Estrutura *raiz, unsigned long long cpf) {
 	if(!raiz) {
-		printf("CPF n„o escontrado!");
+		printf("CPF %llu n√£o encontrado!\n", cpf);
 	
 		return NULL;
 	}
@@ -174,23 +187,21 @@ void update(Estrutura *raiz, unsigned long long cpf) {
 		scanf(" %[^\n]", no->dado.nome);
 		printf("Digite a nova data de nascimento (dd/mm/yyyy): ");
 		scanf(" %[^\n]", no->dado.dataNasc);
-		printf("Digite o novo endereÁo: ");
+		printf("Digite o novo endere√ßo: ");
 		scanf(" %[^\n]", no->dado.endereco);
 		printf("Digite o novo telefone: ");
 		scanf(" %[^\n]", no->dado.telefone);
 	} else {
-		printf("CPF n„o encontrado!\n");
+		printf("CPF %llu n√£o encontrado!\n", cpf);
 	}
 }
 
 void read(Estrutura *raiz) {
-	if (raiz) {
+	if(raiz) {
 		read(raiz->esq);
-		printf("CPF: %llu\n", raiz->dado.cpf);
-		printf("Nome: %s\n", raiz->dado.nome);
-		printf("Data de Nascimento: %s\n", raiz->dado.dataNasc);
-		printf("EndereÁo: %s\n", raiz->dado.endereco);
-		printf("Telefone: %s\n\n", raiz->dado.telefone);
+		printf("CPF: %llu\nNome: %s\nData de Nascimento: %s\nEndere√ßo: %s\nTelefone: %s\n",
+				raiz->dado.cpf, raiz->dado.nome, raiz->dado.dataNasc,
+				raiz->dado.endereco, raiz->dado.telefone);
 		read(raiz->dir);
 	}
 }
@@ -199,7 +210,7 @@ int lerNumero() {
 	int num;
 	
 	while (scanf("%d", &num) != 1) {
-		printf("\n OpÁ„o Inv·lida! Digite um n˙mero: \n");
+		printf("\nOp√ß√£o Inv√°lida! Digite um n√∫mero: \n");
 		
 		while(getchar() != '\n');
 	}
@@ -222,42 +233,88 @@ int main(void) {
 		printf("4 - ATUALIZAR REGISTRO\n");
 		printf("5 - LISTAR REGISTROS\n");
 		printf("0 - SAIR\n\n");
-		printf("Digite a opÁ„o desejada: ");
+		printf("Digite a op√ß√£o desejada: ");
 		op = lerNumero();
 		
 		switch(op) {
 			case 1:{
-				printf("Inserir registro\n");
+				Registro novoRegistro;
+				
+				printf("Digite o CPF: ");
+				scanf("%llu", &novoRegistro.cpf);
+				if(!validarCPF(novoRegistro.cpf)) {
+					printf("CPF inv√°lido!\n");
+					break;
+				}
+				printf("Digite o nome: ");
+				scanf(" %[^\n]", novoRegistro.nome);
+				printf("Digite a data de nascimento (dd/mm/yyyy): ");
+				scanf(" %[^\n]", novoRegistro.dataNasc);
+				printf("Digite o endere√ßo: ");
+				scanf(" %[^\n]", novoRegistro.endereco);
+				printf("Digite o telefone: ");
+				scanf(" %[^\n]", novoRegistro.telefone);
+				
+				raiz = inserir(raiz, novoRegistro);
 				break;
 			}
 			case 2:{
-				printf("Buscar registro\n");
+				unsigned long long cpf;
+				
+				printf("Digite o CPF para buscar: ");
+				while (getchar() != '\n');
+				scanf("&llu", &cpf);
+				
+				Estrutura *no = busca(raiz, cpf);
+				
+				if(no) {
+					printf("\nRegistro encontrado:\n");
+			        printf("CPF: %llu\n", no->dado.cpf);
+			        printf("Nome: %s\n", no->dado.nome);
+			        printf("Data de Nascimento: %s\n", no->dado.dataNasc);
+			        printf("Endere√ßo: %s\n", no->dado.endereco);
+			        printf("Telefone: %s\n", no->dado.telefone);
+				}else {
+					printf("\nCPF %llu n√£o encontrado!\n", cpf);
+				}
 				break;
 			}
 			case 3:{
-				printf("Deletar registro\n");
+				unsigned long long cpf;
+				
+				printf("Digite o CPF para deletar: ");
+				scanf("%llu", &cpf);
+				
+				raiz = remover(raiz, cpf);
 				break;
 			}
 			case 4:{
-				printf("Atualizar registro\n");
+				unsigned long long cpf;
+				
+				printf("Digite o CPF para atualizar: ");
+				scanf("%llu", &cpf);
+				
+				update(raiz, cpf);
 				break;
 			}
 			case 5:{
-				printf("Listar registros\n");
+				printf("Registros cadastrados:\n");
+				
+				read(raiz);
 				break;
 			}
 			case 0:{
-				printf("Sair\n");
+				printf("Saindo...\n");
 				break;
 			}
 			default:{
-				printf("Inv·lido!\n");
+				printf("Op√ß√£o inv√°lida!\n");
 				break;
 			}
 		}
 		
 		if(op!=0) {
-			printf("\n Digite '1' para voltar ao menu ou '0' para encerrar o programa.\n ");
+			printf("\nDigite '1' para voltar ao menu ou '0' para encerrar o programa.\n ");
 			op = lerNumero();
 		}
 		
